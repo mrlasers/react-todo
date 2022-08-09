4e53f0bd691889b891444c971bf54aa848e63195
Timothy Pew <mrlasers@mrlasers.com>
2022-08-09 09:45:49 -0700

    basically redo the interface for the todo stuff
    oops, not really related to storybook integration. i'm bad at git. sorry

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,137 @@
-import { useState, useEffect, useReducer } from 'react'
 // import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
 
-import { Task, PartialTask } from './types'
-import NewTask from './components/NewTask'
-import TaskCards from './components/TaskCards'
-import Navbar from './components/Navbar'
 import { nanoid } from 'nanoid'
+import { useEffect, useReducer, useState } from 'react'
+import { FiDelete, FiMenu, FiPause, FiPlay, FiPlus, FiTrash2, FiWatch, FiX } from 'react-icons/fi'
 
-type State = {
-  nextId: string
-  tasks: Task[]
+export type Project = {
+  id: string
+  title: string
+  description: string
 }
 
-type Action =
-  | { type: 'addTask'; payload: PartialTask }
-  | {
-      type: 'updateTask'
-      payload: Task
-    }
-  | {
-      type: 'deleteTask'
-      payload: string | null
-    }
-
-const initialState = localStorage.getItem('todo-state')
-
-const stateReducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    default:
-      return state
-    case 'addTask':
-      return {
-        ...state,
-        nextId: nanoid(),
-        tasks: [
-          ...state.tasks,
-          {
-            description: '',
-            dueDate: '',
-            status: 'incomplete',
-            assignedTo: '',
-            id: state.nextId,
-            createdAt: new Date().toISOString(),
-            ...action.payload,
-          },
-        ],
-      }
-    case 'updateTask':
-      return {
-        ...state,
-        tasks: state.tasks.map((task) => {
-          return task.id === action.payload.id ? action.payload : task
-        }),
-      }
-    case 'deleteTask':
-      return action.payload === null
-        ? { ...state, tasks: [] }
-        : {
-            ...state,
-            tasks: state.tasks.filter((task) => task.id !== action.payload),
-          }
-  }
-}
-
-function App() {
-  const [state, dispatch] = useReducer(
-    stateReducer,
-    initialState ? JSON.parse(initialState) : { nextId: 0, tasks: [] }
-  )
+function useTodoProjects() {
+  const [projects, setProjects] = useState<Project[]>([])
 
   useEffect(() => {
-    localStorage.setItem('todo-state', JSON.stringify(state))
-  }, [state])
+    setProjects([
+      {
+        id: nanoid(),
+        title: 'Untitled Project 1',
+        description: '',
+      },
+    ])
+  }, [])
 
-  const addNewTask = (task: PartialTask) => {
-    dispatch({ type: 'addTask', payload: task })
+  function addNewProject() {
+    setProjects([
+      ...projects,
+      {
+        id: nanoid(),
+        title: 'Untitled Project ' + (projects.length + 1),
+        description: '',
+      },
+    ])
   }
 
-  const updateTask = (task: Task) => {
-    dispatch({ type: 'updateTask', payload: task })
-  }
+  return { addNewProject, projects }
+}
 
-  const deleteTask = (id: string | null) => {
-    dispatch({ type: 'deleteTask', payload: id })
-  }
+const App = () => {
+  const { projects, addNewProject } = useTodoProjects()
 
   return (
     <>
       <header>
-        <Navbar onDeleteAll={() => deleteTask(null)} />
+        <nav>
+          <h1>Project Tracker</h1>
+        </nav>
       </header>
-      <main className='App'>
-        <NewTask onNewTask={addNewTask} />
-        <TaskCards
-          tasks={state.tasks}
-          onDelete={deleteTask}
-          onChange={updateTask}
-        />
+      <main>
+        <section className='project-card'>
+          <div className='header'>
+            <div>
+              <input
+                type='text'
+                className='project-title'
+                value='Project Title'
+              />
+            </div>
+            <div className='button'>
+              <FiMenu />
+            </div>
+          </div>
+          <div className='description'>
+            <textarea spellCheck={false}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Perferendis dolores laborum illo fuga, suscipit ut in! Ea possimus
+              nesciunt fugit incidunt quae eum iste. Reprehenderit esse
+              consequatur, porro deserunt fugiat omnis corporis? Exercitationem,
+              deleniti? Excepturi, recusandae. Repellendus porro possimus vitae
+              vero iure nulla provident nam?
+            </textarea>
+          </div>
+        </section>
+        <section className='task-list'>
+          <ul>
+            <li className='active'>
+              {/* <h3 className='title'>Work on todo app</h3> */}
+              <input
+                className='title'
+                type='text'
+                defaultValue='Work on some todo app'
+              />
+              <div className='time'>
+                3 <FiWatch /> 0:15:32
+              </div>
+              <nav className='task-ctrls'>
+                <span className='button'>
+                  <FiX />
+                </span>
+                <span className='button'>
+                  <FiPause />
+                </span>
+                {/* <span className='button'>
+                  <FiPlay />
+                </span> */}
+              </nav>
+            </li>
+            <li className='new-task'>
+              <input type='text' className='title' />
+              <nav className='task-ctrls'>
+                <span className='button' title='Add task'>
+                  <FiPlus />
+                </span>
+                <span
+                  className='button'
+                  title='Add task and start time tracking'>
+                  <FiPlay />
+                </span>
+              </nav>
+            </li>
+            <li>
+              <input
+                className='title'
+                type='text'
+                defaultValue='Reply to emails'
+              />
+              {/* <h3 className='title'>Reply to emails</h3> */}
+              <div className='time'>
+                0 <FiWatch /> -:--:--
+              </div>
+              <nav className='task-ctrls'>
+                <span className='button'>
+                  <FiTrash2 />
+                </span>
+                <span className='button'>
+                  <FiPlay />
+                </span>
+              </nav>
+            </li>
+          </ul>
+        </section>
       </main>
-      <footer>
-        <a href='https://www.flaticon.com/free-icons/to-do' title='to-do icons'>
-          To-do icons created by Freepik - Flaticon
-        </a>
-      </footer>
     </>
   )
 }
